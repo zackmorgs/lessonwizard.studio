6311b440e7e4cef825b9009e3025bd0b991eaa15
zackmorgs <zackmorgenthaler@gmail.com>
2026-06-10 12:18:00 -0500

    feat: improve layout and styling for Home page; enhance button interactions

--- src/client/src/pages/Home.jsx
+++ src/client/src/pages/Home.jsx
@@ -14,35 +14,39 @@ export default function Home() {
             alt="zoilerplate logo"
             className="mb-4"
           /> */}
-          <h1 className="title text-4xl font-semibold mb-4">lessonwizard.studio</h1>
+          <h1 className="title text-4xl font-semibold mb-4">
+            lessonwizard.studio
+          </h1>
           <p className="mt-2 mb-4 text-lg">
             A free and open-source lesson planning tool for teachers, by
             teachers.
           </p>
-          <Link
-            to="/login"
-            className="btn btn-primary btn-lg mt-8"
-            alt="Login button"
-          >
-            <img
-              src="/assets/svg/icon-login.svg"
-              alt="Login Icon"
-              className="icon"
-            />
-            <span className="btn-text">Login</span>
-          </Link>
-          <Link
-            to="/signup"
-            className="btn btn-primary btn-lg mt-6"
-            alt="Sign Up button"
-          >
-            <img
-              src="/assets/svg/icon-signup.svg"
-              alt="Sign Up Icon"
-              className="icon"
-            />
-            <span className="btn-text">Sign Up</span>
-          </Link>
+          <div className="flex flex-col items-center justify-center">
+            <Link
+              to="/login"
+              className="btn btn-primary btn-lg mt-8"
+              alt="Login button"
+            >
+              <img
+                src="/assets/svg/icon-login.svg"
+                alt="Login Icon"
+                className="icon"
+              />
+              <span className="btn-text">Login</span>
+            </Link>
+            <Link
+              to="/signup"
+              className="btn btn-primary btn-lg mt-6"
+              alt="Sign Up button"
+            >
+              <img
+                src="/assets/svg/icon-signup.svg"
+                alt="Sign Up Icon"
+                className="icon"
+              />
+              <span className="btn-text">Sign Up</span>
+            </Link>
+          </div>
         </div>
       </header>
     </Layout>

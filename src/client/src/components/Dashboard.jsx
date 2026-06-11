7edf51cd34c614a7376201b583eacd991814e871
zackmorgs <zackmorgenthaler@gmail.com>
2026-06-11 07:24:53 -0500

    feat: add TodayView component to display today's lessons in Dashboard

--- src/client/src/components/Dashboard.jsx
+++ src/client/src/components/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Layout from "../components/Layout";
+import TodayView from "./TodayView";
 
 export default function Dashboard() {
     return (
         <>
             <h1>Dashboard</h1>
+            <TodayView />
         </>
     );
 }
---
+++ src/client/src/components/TodayView.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+export default function TodayView() {
+        return (
+            <>
+                <section id="today_view">
+                    <h2>Today's Lessons</h2>
+                    <p>You have no lessons scheduled for today.</p>
+                </section>
+            </>
+        );
+
+}

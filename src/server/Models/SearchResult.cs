939d15349212955eddfb450a188b6f2ea0968538
zackmorgs <zackmorgenthaler@gmail.com>
2026-06-16 13:02:08 -0500

    feat: add JsonPropertyName attributes to SearchResult properties for improved serialization

--- src/server/Models/SearchResult.cs
+++ src/server/Models/SearchResult.cs
@@ -1,9 +1,18 @@
+using System.Text.Json.Serialization;
+
 namespace Models;
 
 public class SearchResult
 {
+    [JsonPropertyName("id")]
     public string Id { get; set; } = string.Empty;
+
+    [JsonPropertyName("title")]
     public string Title { get; set; } = string.Empty;
+
+    [JsonPropertyName("type")]
     public string Type { get; set; } = string.Empty;
+
+    [JsonPropertyName("spotifyTrackId")]
     public string? SpotifyTrackId { get; set; }
 }

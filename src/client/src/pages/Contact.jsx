0c3ce3ccc594a0829603bd864011310954715146
zackmorgs <zackmorgenthaler@gmail.com>
2026-06-19 05:57:58 -0500

    style: enhance layout and formatting of About and Contact pages for improved readability

--- src/client/src/pages/Contact.jsx
+++ src/client/src/pages/Contact.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import Layout from '../components/Layout';
+import React from "react";
+import Layout from "../components/Layout";
 
 export default function Contact() {
   return (
     <Layout>
+      <div className="md:max-w-4xl mx-auto">
         <header className="header">
           <div className="p-4">
             <h1 className="text-4xl font-semibold text-center">Contact</h1>
@@ -11,9 +12,21 @@ export default function Contact() {
         </header>
         <section className="section">
           <div className="panel">
-            <p>This project was created with love by <a href="https://www.zackmorgenthaler.com" target="_blank" rel="noopener noreferrer" className="text-link">Zack Morgenthaler</a>.</p>
+            <p>
+              This project was created with love by{" "}
+              <a
+                href="https://www.zackmorgenthaler.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-link"
+              >
+                Zack Morgenthaler
+              </a>
+              .
+            </p>
           </div>
         </section>
+      </div>
     </Layout>
   );
 }

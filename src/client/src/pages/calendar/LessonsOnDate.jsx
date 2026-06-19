e88b4c78c4d8527b911c86e76ebf35844f9ac35c
zackmorgs <zackmorgenthaler@gmail.com>
2026-06-19 14:00:31 -0500

    style: improve date formatting and layout in LessonsOnDate component

--- src/client/src/pages/calendar/LessonsOnDate.jsx
+++ src/client/src/pages/calendar/LessonsOnDate.jsx
@@ -7,27 +7,38 @@ import Layout from "../../components/Layout";
 import LessonList from "../../components/LessonList";
 
 export default function LessonsOnDate() {
-	const { year, month, day } = useParams();
-    const [lessons, setLessons] = React.useState([]);
+  const { year, month, day } = useParams();
+  const [lessons, setLessons] = React.useState([]);
 
-    React.useEffect(() => {
-		const selectedDate = `${year}-${String(month).padStart(2, "0")}-${String(day).padStart(2, "0")}`;
+  const prettyDate = new Date(year, month - 1, day).toLocaleDateString(
+    "en-US",
+    {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    },
+  );
 
-		getLessonsOnDate(selectedDate)
-            .then(setLessons)
-            .catch(() => setLessons([]));
-    }, [year, month, day]);
+  React.useEffect(() => {
+    const selectedDate = `${year}-${String(month).padStart(2, "0")}-${String(day).padStart(2, "0")}`;
 
-	return (
-		<Layout>
-			<div className="md:max-w-md mx-auto">
-				<section className="section">
-					<div className="panel">
-						<h1 className="h1">Lessons on {`${year}-${month}-${day}`}</h1>
-					</div>
-				</section>
-				<LessonList lessons={lessons} />
-			</div>
-		</Layout>
-	);
+    getLessonsOnDate(selectedDate)
+      .then(setLessons)
+      .catch(() => setLessons([]));
+  }, [year, month, day]);
+
+  return (
+    <Layout>
+      <div className="md:max-w-md mx-auto">
+        <section className="section">
+          <div className="panel">
+            <h1 className="h1 md:text-xl">
+              Lessons on <span className="date">{prettyDate}</span>
+            </h1>
+          </div>
+        </section>
+        <LessonList lessons={lessons} />
+      </div>
+    </Layout>
+  );
 }

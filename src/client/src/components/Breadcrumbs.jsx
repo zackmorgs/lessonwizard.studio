098e966c52f71dbcb7562b45d638530b7109415d
zackmorgs <zackmorgenthaler@gmail.com>
2026-06-22 11:22:29 -0500

    feat: update Breadcrumbs component layout with custom max-width and centering

--- src/client/src/components/Breadcrumbs.jsx
+++ src/client/src/components/Breadcrumbs.jsx
@@ -5,7 +5,7 @@ import {Link} from 'react-router';
 export default function Breadcrumbs({ to, label }) {
   return (
     <div className="breadcrumbs">
-      <div className="w-max-xl">
+      <div className="max-w-custom mx-auto">
         <nav className="panel">
           <Link to={to} className="flex flex-row items-center">
             <svg className="icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z"/></svg>
